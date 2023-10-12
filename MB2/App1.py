c09a347275586496af4a9c07c99b37918d2dd131
Elayaraja <kelaya5@gmail.com>
2023-10-12 13:14:50 +0530

    Add files via upload

---
+++ MB2/App1.py
@@ -0,0 +1,29 @@
+name="JSON JavaScript Angular"
+print(name[5])
+#print(name[13])
+lastIndex=len(name)-1
+mid=int(len(name)/2)
+print("The last index is : ",lastIndex," values : ",name[lastIndex])
+print("The last index is : ",mid," values : ",name[mid])
+lang="java"
+index=0
+for temp in lang:
+	print("The index is ",index,"value : ",temp)
+	#index = index+1
+	index +=1
+'''
+Num1=10
+Num2=20
+temp=Num1 #temp = 10 
+Num1=Num2=#Num1=20
+Num2=temp#Num2=
+'''
+
+Num1=100
+Num2=200
+print("Before swap : Num1 Val : ",Num1," Num 2 Val : ",Num2)
+Num1 = Num1 + Num2 #Num1 = 300
+Num2 = Num1-Num2#Num2 =100
+Num1 = Num1-Num2 #300 - 100
+print("After swap : Num1 Val : ",Num1," Num 2 Val : ",Num2)
+   
---
+++ MB2/App2.py
@@ -0,0 +1,7 @@
+Name="Angular,javaScript,Java and python"
+
+print(Name[3:10])
+print(len(Name))
+print(Name[4:25:3])
+#[startIndex:endIndex:jumpIndex]
+print(Name[len(Name):0:-1])
